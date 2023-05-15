bfc9482dab176081f78086e5ffd1db0b8577a3df
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-14 22:36:58 -0300

    Create -> Belonging and GameList Entities, relacionship(Belonging-GamaList), BelongingPK(Primary Key), GameAllDTO(Return filds necessaries), GamesService/GamesResource(API REST) Update -> GameRepository, GameResouce

---
+++ src/main/java/com/example/games/entities/Belonging.java
@@ -0,0 +1,26 @@
+package com.example.games.entities;
+
+import com.example.games.entities.pk.BelongingPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_belonging")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Belonging {
+    @EmbeddedId
+    private BelongingPK id;
+    private Integer position;
+
+    public Belonging(Game game, GameList list, Integer position){
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+}
