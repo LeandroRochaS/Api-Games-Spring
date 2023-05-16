fb63d1e4947adeb507c08d3dad1bbc03f0a5a6c0
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-15 22:15:55 -0300

    Deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks.withType<Test> {
 tasks.jar {
 	enabled = true
 	manifest {
-		attributes["Main-Class"] = "com.example.games"
+		attributes["Main-Class"] = "com.example.games.Application"
 	}
 }
 
